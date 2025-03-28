761f8cd7e1be78b6d4eec0fd42078e706969f8a7
Lyzza Abig <lrs.abig@yahoo.com>
2025-03-28 21:24:08 +0800

    Change script

--- script.js
+++ script.js
@@ -3,25 +3,50 @@ const modal = document.getElementById('imageModal');
 const modalImage = document.getElementById('modalImage');
 const closeModal = document.getElementById('closeModal');
 let zoomLevel = 1;
+let isDragging = false;
+let startX, startY, currentX = 0, currentY = 0;
+
 
 thumbnails.forEach(thumbnail => {
   thumbnail.addEventListener('click', () => {
     modalImage.src = thumbnail.src;
     modal.style.display = 'flex';
     zoomLevel = 1;
-    modalImage.style.transform = `scale(${zoomLevel})`;
+    modalImage.style.transform = `scale(${zoomLevel}) translate(0px, 0px)`;
   });
 });
 
-// Close modal
 closeModal.addEventListener('click', () => {
   modal.style.display = 'none';
 });
-
 
 modalImage.addEventListener('wheel', (event) => {
   event.preventDefault();
   zoomLevel += event.deltaY * -0.01;
   zoomLevel = Math.min(Math.max(zoomLevel, 1), 3);
-  modalImage.style.transform = `scale(${zoomLevel})`;
+  modalImage.style.transform = `scale(${zoomLevel}) translate(${currentX}px, ${currentY}px)`;
+});
+
+modalImage.addEventListener('mousedown', (event) => {
+  isDragging = true;
+  startX = event.clientX - currentX;
+  startY = event.clientY - currentY;
+  modalImage.style.cursor = 'grabbing';
+});
+
+modalImage.addEventListener('mousemove', (event) => {
+  if (!isDragging) return;
+  currentX = event.clientX - startX;
+  currentY = event.clientY - startY;
+  modalImage.style.transform = `scale(${zoomLevel}) translate(${currentX}px, ${currentY}px)`;
+});
+
+modalImage.addEventListener('mouseup', () => {
+  isDragging = false;
+  modalImage.style.cursor = 'grab';
+});
+
+modalImage.addEventListener('mouseleave', () => {
+  isDragging = false;
+  modalImage.style.cursor = 'grab';
 });
